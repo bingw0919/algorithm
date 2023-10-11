43b4bf5b3459c025b46393e4983ab80391409575
bingw0919 <wbpro0919@gmail.com>
2023-10-11 20:30:41 +0800

    冒泡排序和选择排序

---
+++ p3/Code02_SelectionSort.java
@@ -0,0 +1,44 @@
+package p3;
+
+import java.util.Random;
+
+public class Code02_SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = new int[10];
+        Random random = new Random();
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100);
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        sort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    /**
+     * 选择排序
+     *
+     * @param arr 数组
+     */
+    public static void sort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[minIndex] > arr[j]) {
+                    minIndex = j;
+                }
+            }
+            swap(arr, minIndex, i);
+        }
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int num = arr[i];
+        arr[i] = arr[j];
+        arr[j] = num;
+    }
+}
