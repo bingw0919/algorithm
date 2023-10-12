5c08c7d5fa0d6087d73bc39402639b74ea3d78cf
bingw0919 <wbpro0919@gmail.com>
2023-10-12 19:30:25 +0800

    数组中只出现奇数次的数字

---
+++ src/p3/Code03_OddTimes.java
@@ -0,0 +1,21 @@
+package p3;
+
+
+public class Code03_OddTimes {
+    /**
+     * 只出现奇数次的数字
+     * @param arr 数组
+     */
+    public static void print(int[] arr) {
+        int a = 0;
+        for (int i : arr) {
+            a = a ^ i;
+        }
+        System.out.println(a);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 6, 2, 9, 3, 1, 2, 6, 3};
+        print(arr);
+    }
+}
