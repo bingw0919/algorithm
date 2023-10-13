066944320bb771e55fe51a15b017698cd1701805
bingw0919 <wbpro0919@gmail.com>
2023-10-13 19:45:02 +0800

    插入排序

--- src/p3/Code05_InsertSort.java
+++ src/p3/Code05_InsertSort.java
@@ -1,2 +1,41 @@
-package p3;public class Code05_InsertSort {
+package p3;
+
+import java.util.Random;
+
+public class Code05_InsertSort {
+    public static void main(String[] args) {
+        int[] arr = new int[10];
+        Random random = new Random();
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100);
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        sort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    /**
+     * 插入排序
+     *
+     * @param arr 数组
+     */
+    public static void sort(int[] arr) {
+        if (arr == null || arr.length < 2) return;
+        for (int i = 1; i < arr.length; i++) {//对0~i近进行排序
+            for (int j = i - 1; j >= 0 && arr[j] > arr[j + 1]; j--) {//前i-1个数据有序，寻找第i个数合适的插入位置
+                swap(arr, j, j + 1);
+            }
+        }
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int num = arr[i];
+        arr[i] = arr[j];
+        arr[j] = num;
+    }
 }
