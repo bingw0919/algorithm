6fb920844fd0b9ac863d4ce0197d179cb8a6f210
bingw0919 <wbpro0919@gmail.com>
2023-10-22 13:26:37 +0800

    有序数组中寻找最右侧<=num的数

--- src/class03/Code03_BSNearRight.java
+++ src/class03/Code03_BSNearRight.java
@@ -3,15 +3,16 @@ package class03;
 public class Code03_BSNearRight {
     /**
      * 有序数组中寻找最右侧<=num的数
+     *
      * @param arr 数组
      * @param num 待查数字
      * @return 结果
      */
-    public static int mostLeftNoLessNumIndex(int[] arr, int num) {
+    public static int nearestIndex(int[] arr, int num) {
         if (arr == null || arr.length == 0) return -1;
         int L = 0, R = arr.length - 1, mid, ans = -1;
         while (L <= R) {
-            mid = (L + R) / 2;
+            mid = L + (R - L) / 2;
             if (arr[mid] <= num) {
                 ans = mid;
                 L = mid + 1;
@@ -23,8 +24,9 @@ public class Code03_BSNearRight {
     }
 
     public static void main(String[] args) {
-        int[] arr = {1, 2, 2, 2, 3, 4, 4, 5, 5, 6, 7};
-        System.out.println(mostLeftNoLessNumIndex(arr, 3));//3
-        System.out.println(mostLeftNoLessNumIndex(arr, 5 ));//6
+        int[] arr = {1, 2, 2, 2, 3, 4, 4, 5, 5, 6, 7, 9};
+        System.out.println(nearestIndex(arr, 3));//3
+        System.out.println(nearestIndex(arr, 5));//6
+        System.out.println(nearestIndex(arr, 8));//10
     }
 }
