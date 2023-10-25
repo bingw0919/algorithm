57c9ef57411551a595cb76e7892126f4caa7dd62
bingw0919 <wbpro0919@gmail.com>
2023-10-25 10:06:50 +0800

    合并两个有序链表

--- src/zuochengyun_new/class04/Code08_MergeTwoSortedLinkedList.java
+++ src/zuochengyun_new/class04/Code08_MergeTwoSortedLinkedList.java
@@ -1,7 +1,7 @@
-package zuochengyun_new.example.class04;
+package zuochengyun_new.class04;
 
 // 测试链接：https://leetcode.com/problems/merge-two-sorted-lists
-public class Code06_MergeTwoSortedLinkedList {
+public class Code08_MergeTwoSortedLinkedList {
 
 	// 不要提交这个类
 	public static class ListNode {
@@ -10,25 +10,22 @@ public class Code06_MergeTwoSortedLinkedList {
 	}
 
 	public static ListNode mergeTwoLists(ListNode head1, ListNode head2) {
-		if (head1 == null || head2 == null) {
-			return head1 == null ? head2 : head1;
-		}
-		ListNode head = head1.val <= head2.val ? head1 : head2;
-		ListNode cur1 = head.next;
-		ListNode cur2 = head == head1 ? head2 : head1;
-		ListNode pre = head;
-		while (cur1 != null && cur2 != null) {
-			if (cur1.val <= cur2.val) {
-				pre.next = cur1;
-				cur1 = cur1.next;
-			} else {
-				pre.next = cur2;
-				cur2 = cur2.next;
+		if(head1==null) return head2;
+		if (head2==null) return head1;
+		ListNode head = new ListNode();
+		ListNode cur=head;
+		while (head1!=null&&head2!=null){
+			if (head1.val< head2.val){
+				cur.next=head1;
+				head1=head1.next;
+			}else {
+				cur.next=head2;
+				head2=head2.next;
 			}
-			pre = pre.next;
+			cur=cur.next;
 		}
-		pre.next = cur1 != null ? cur1 : cur2;
-		return head;
+		cur.next=(head1==null)?head2:head1;
+		return head.next;
 	}
 
 }
