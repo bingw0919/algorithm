fdfa97c5642277a50e7cac42d32ece55858f57ae
bingw0919 <wbpro0919@gmail.com>
2023-11-09 13:20:06 +0800

    路径总和-解法1

---
+++ src/zuochengyun_new/class07/Code04_PathSum.java
@@ -0,0 +1,18 @@
+package zuochengyun_new.class07;
+
+public class Code04_PathSum {
+
+    // 测试链接：https://leetcode.com/problems/path-sum
+    public static class TreeNode {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+
+
+}
