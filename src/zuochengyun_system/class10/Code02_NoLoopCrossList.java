12c73e48c0a4e786ef204203aabeebf210dda96a
bingw0919 <wbpro0919@gmail.com>
2023-12-19 15:00:57 +0800

    相交链表-双指针

--- src/zuochengyun_system/class10/Code02_NoLoopCrossList.java
+++ src/zuochengyun_system/class10/Code02_NoLoopCrossList.java
@@ -16,19 +16,57 @@ public class Code02_NoLoopCrossList {
             next = null;
         }
     }
+
     public ListNode getIntersectionNode1(ListNode headA, ListNode headB) {
         if (headA == null || headB == null) {
             return null;
         }
         HashSet hashSet = new HashSet();
-        while (headA!=null){
+        while (headA != null) {
             hashSet.add(headA);
-            headA=headA.next;
+            headA = headA.next;
         }
-        while (headB!=null){
-            if(hashSet.contains(headB))  return headB;
-            headB=headB.next;
+        while (headB != null) {
+            if (hashSet.contains(headB)) return headB;
+            headB = headB.next;
         }
         return null;
+    }
+
+    public ListNode getIntersectionNode2(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+        ListNode cur1 = headA, cur2 = headB;
+        int len = 0;
+        while (cur1.next != null) {
+            len++;
+            cur1 = cur1.next;
+        }
+        while (cur2.next != null) {
+            len--;
+            cur2 = cur2.next;
+        }
+        if (cur1 != cur2) return null;
+        cur1 = headA;
+        cur2 = headB;
+        if (len > 0) {
+            len = Math.abs(len);
+            while (len != 0) {
+                cur1 = cur1.next;
+                len--;
+            }
+        } else {
+            len = Math.abs(len);
+            while (len != 0) {
+                cur2 = cur2.next;
+                len--;
+            }
+        }
+        while (cur1 != cur2) {
+            cur1 = cur1.next;
+            cur2 = cur2.next;
+        }
+        return cur1;
     }
 }
